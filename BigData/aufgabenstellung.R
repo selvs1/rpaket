4da4ab334ef552b15d0af41e0b4642d2856cb346
selvs1 <42969019+selvs1@users.noreply.github.com>
2020-05-08 15:48:58 +0200

    Bigdata R.Script erstellt

---
+++ BigData/aufgabenstellung.R
@@ -0,0 +1,16 @@
+#Aufgabenstellung: 
+  
+#Möglichkeiten in R mit großen Datenmengen umzugehen zeigen und dabei klären, 
+# was Big Data ist
+
+#Für folgende Daten https://www.kaggle.com/azathoth42/myanimelist#UserAnimeList.csv 
+# zeigen Sie, 
+# wie man diese auswertbar macht mit R-Big Data Funktionen. 
+# Machen Sie dazu beschreibende Statistiken
+
+#Links: 
+#  https://rstudio.com/resources/webinars/working-with-big-data-in-r/
+#  https://rpubs.com/msundar/large_data_analysis
+
+
+# Kaggle Dataset ist 9 GB gross!!
