2d7d97a3d8764d912c9a35afdae89e50d464354e
selvs1 <42969019+selvs1@users.noreply.github.com>
2020-05-30 16:21:51 +0200

    ff read geit no nid

--- BigData/aufgabenstellung.R
+++ BigData/aufgabenstellung.R
@@ -14,3 +14,147 @@
 
 
 # Kaggle Dataset ist 9 GB gross!!
+
+###################################################################################
+
+
+#install.packages("ff")
+#install.packages("ffbase")
+library(ff)
+library(ffbase)
+
+## ff Vorbereiten
+sourcePath <- "D:/Users/sugeelan/RProject_Anime/myanimelist/UserAnimeList.csv"
+tempPath <- "D:/Users/sugeelan/RProject_Anime/TEMP"
+tempPath <- tempfile()
+
+## Check Temp path
+getOption("fftempdir")
+
+## set Temp path
+options("fftempdir" = tempPath)
+
+## Check Temp path again
+getOption("fftempdir")
+
+################################################################################### skippable
+
+
+# creating the file
+anime.ffdf <- read.csv.ffdf(file=sourcePath) # Achtung dauert 8 Minuten
+#anime.ffdf <- as.ffdf(iris)
+
+# creates a ffdf object
+class(anime.ffdf)
+
+
+# ffdf is a virtual dataframe
+str(anime.ffdf)
+
+# ffdf object can be treated as any other R object
+sum(anime.ffdf[,3])
+
+
+####### Test
+#iris.ffdf <- as.ffdf(iris)
+
+#td <- tempfile()
+#td <- tempPath
+
+# save the ffdf into the supplied directory
+#save.ffdf(iris.ffdf, dir=td)
+
+
+# what in the directory?
+#dir(td)
+
+#remove the ffdf from memory
+#rm("iris.ffdf")
+
+# and reload the stored ffdf
+#load.ffdf(dir=td)
+
+#tf <- paste(td, ".zip", sep="")
+#close(iris.ffdf)
+#packed <- pack.ffdf(file=tf, iris.ffdf)
+
+#remove the ffdf from memory
+#rm("iris.ffdf")
+
+# restore the ffdf from the packed ffdf
+#unpack.ffdf(tf)
+
+#iris.ffdf
+
+####### \test
+
+
+
+#################################################################################### skippable- Save
+
+getOption("fftempdir")
+
+
+dir(tempPath)
+
+# save the ffdf into the supplied directory
+save.ffdf(anime.ffdf, dir = tempPath)
+#finalize.ffdf()
+
+# what in the directory?
+dir(tempPath)
+
+# remove ffdf from memory
+rm(anime.ffdf)
+
+
+
+
+#################################################################################### skippable- Load
+
+# and reload the stored ffdf
+load.ffdf(dir = tempPath)
+
+outPath <- "D:/Users/sugeelan/RProject_Anime/Out1"
+
+tf <- paste(outPath, ".zip", sep = "")
+close(anime.ffdf)
+is.open(anime.ffdf)
+packed <- pack.ffdf(file = tf, anime.ffdf)
+
+
+
+#remove the ffdf from memory
+rm("anime.ffdf")
+
+# restore the ffdf from the packed ffdf
+unpack.ffdf(tf)
+
+anime.ffdf
+
+
+
+###################################################################################
+
+
+aData <- anime.ffdf
+
+## Wie sieht der Data aus?
+View(aData)
+
+## Dataframe? sieht hässlich aus
+str(aData)
+
+# Dimension
+dim(aData)
+
+# Spalten
+names(aData)
+
+
+str(anime.ffdf[1:10,])
+
+
+
+
+
